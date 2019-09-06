4652b150110066a6b1f014205b2274c6150f82a9
= <=>
2019-09-06 11:10:46 +0800

    第六次提交完善点击列表跳转页面

--- js/index.js
+++ js/index.js
@@ -1,26 +1,23 @@
 $(function(){
-	
+	// 进入页面自动加载首页
+	$(".mb_nav").css('display', 'none');
+	$("#wrapper").load("./pages/homepage.html");
+
+	// 点击首页标题出现首页页面
+	$(".title").on("click",function(){
+		$(".mb_nav").css('display', 'none');
+		var url = $(this).attr("url");
+		$(".left_nav>li").removeClass("current");
+		$("#wrapper").load(url);
+	})
+
 	// 点击列表时跳转页面
 	$("ul.left_nav").on("click","li",function(){
+		$(".mb_nav").css('display', 'block');
 		var url = $(this).attr("url");
 		$(".left_nav>li").removeClass("current");
 		$(this).addClass("current");
 		$("#wrapper").load(url);
 	})
-
-	// 模拟点击第一个li
-	$("ul.left_nav>li:first-child").trigger('click');
-
-
-
-	/*$(".mb_nav").css('display', 'none');
-	$("#wrapper").css('display', 'none');*/
-	// $(".content").load("./pages/homepage.html");
-
-	/*// 当鼠标点击列表时跳转
-	// 首页
-	$(".title").click(function(){
-		$(".content").load("./pages/homepage.html");
-	})*/
 
 });
