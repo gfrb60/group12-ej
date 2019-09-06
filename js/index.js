d132e955ee006cd1cf67e91a26b4782f8a288446
= <=>
2019-09-06 10:48:28 +0800

    第五次提交优化列表点击样式

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 $(function(){
-	$("#wrapper").load("./pages/customer.html");
+	
+	// 点击列表时跳转页面
+	$("ul.left_nav").on("click","li",function(){
+		var url = $(this).attr("url");
+		$(".left_nav>li").removeClass("current");
+		$(this).addClass("current");
+		$("#wrapper").load(url);
+	})
+
+	// 模拟点击第一个li
+	$("ul.left_nav>li:first-child").trigger('click');
+
+
+
 	/*$(".mb_nav").css('display', 'none');
 	$("#wrapper").css('display', 'none');*/
 	// $(".content").load("./pages/homepage.html");
@@ -9,47 +22,5 @@ $(function(){
 	$(".title").click(function(){
 		$(".content").load("./pages/homepage.html");
 	})*/
-
-	// 点击顾客管理选项列表时
-	$(".btn_customer").click(function(){
-		$(".left_nav>li").removeClass("current");
-		$(".left_nav>li:nth-child(1)").addClass("current");
-		$("#wrapper").load("./pages/customer.html");
-	})
-
-	// 点击员工管理选项列表时
-	$(".btn_waiter").click(function(){
-		$(".left_nav>li").removeClass("current");
-		$(".left_nav>li:nth-child(2)").addClass("current");
-		$("#wrapper").load("./pages/waiter.html");
-	})
-
-	// 点击分类管理选项列表时
-	$(".btn_category").click(function(){
-		$(".left_nav>li").removeClass("current");
-		$(".left_nav>li:nth-child(3)").addClass("current");
-		$("#wrapper").load("./pages/category.html");
-	})
-
-	// 点击产品管理选项列表时
-	$(".btn_product").click(function(){
-		$(".left_nav>li").removeClass("current");
-		$(".left_nav>li:nth-child(4)").addClass("current");
-		$("#wrapper").load("./pages/product.html");
-	})
-
-	// 点击订单管理选项列表时
-	$(".btn_order").click(function(){
-		$(".left_nav>li").removeClass("current");
-		$(".left_nav>li:nth-child(5)").addClass("current");
-		$("#wrapper").load("./pages/order.html");
-	})
-
-	// 点击评论管理选项列表时
-	$(".btn_comment").click(function(){
-		$(".left_nav>li").removeClass("current");
-		$(".left_nav>li:nth-child(6)").addClass("current");
-		$("#wrapper").load("./pages/comment.html");
-	})
 
 });
